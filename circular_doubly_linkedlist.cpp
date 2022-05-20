2b4c2b32f87a96e936ab9c7d560a4cd6248c7e46
yadhumanikandan <yadhumanikandan0@gmail.com>
2022-05-20 16:30:32 +0530

    basic things

--- circular_doubly_linkedlist.cpp
+++ circular_doubly_linkedlist.cpp
@@ -1,8 +1,69 @@
 #include<iostream>
 using namespace std;
 
+struct node{
+    node *prev;
+    int data;
+    node *nest;
+};
+
+int count = 0, pos, val;
+node *head = NULL, *temp, *ptr;
+
+void insrtBig(){}
+
+void insrtEnd(){}
+
+void insrtReq(){}
+
+void delBeg(){}
+
+void delEnd(){}
+
+void delReq(){}
+
+void display(){}
+
 int main(){
-    cout<<"this is circular doubly linked list";
+
+    char choice;
+
+    do{
+    cout<<"\n\n\n  *SELECT AN OPERATION*\n  (current length> "<<count<<")\n\n1-> Insert at begining\n2-> Insert at end\n3-> insert at required location\n4-> Delete from begining\n5-> Delete from end\n6-> Delete from required location\n7-> Display the list\n0-> Exit\n\n-> ";
+    cin>>choice;
+
+    switch (choice)
+    {
+    case '1': insrtBig();
+        break;
+
+    case '2': insrtEnd();
+        break;
+
+    case '3': insrtReq();
+        break;
+
+    case '4': delBeg();
+        break;
+
+    case '5': delEnd();
+        break;
+
+    case '6': delReq();
+        break;
+
+    case '7': display();
+        break;
+
+    case '0': cout<<"\nExiting program....\n\n";
+        break;
+    
+    default:cout<<"\n\n**Enter a valied choice**";
+        break;
+    }
+
+    }while (choice!='0');
+
 
     return 0;
 }
